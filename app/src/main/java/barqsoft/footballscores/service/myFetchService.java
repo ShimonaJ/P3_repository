10a8975d42633c191dbd647df9859287042321db
shimonaj <shimona.khandelwal@gmail.com>
2016-02-08 19:20:29 +0530

    Implemented Material

--- app/src/main/java/barqsoft/footballscores/service/myFetchService.java
+++ app/src/main/java/barqsoft/footballscores/service/myFetchService.java
@@ -47,6 +47,7 @@ public class myFetchService extends IntentService
 
     private void getData (String timeFrame)
     {
+
         //Creating fetch URL
         final String BASE_URL = "http://api.football-data.org/alpha/fixtures"; //Base URL
         final String QUERY_TIME_FRAME = "timeFrame"; //Time Frame parameter to determine days
@@ -54,7 +55,7 @@ public class myFetchService extends IntentService
 
         Uri fetch_build = Uri.parse(BASE_URL).buildUpon().
                 appendQueryParameter(QUERY_TIME_FRAME, timeFrame).build();
-        //Log.v(LOG_TAG, "The url we are looking at is: "+fetch_build.toString()); //log spam
+        Log.v(LOG_TAG, "The url we are looking at is: "+fetch_build.toString()); //log spam
         HttpURLConnection m_connection = null;
         BufferedReader reader = null;
         String JSON_data = null;
